1d8d998849914fb02b29df6dea4e2174659fe823
Sergio Filipe <sergio.filipe@evolux.net.br>
2020-02-23 15:29:06 -0300

    Adding a timeout to detect end of a packet

--- lib/switchx/connection/socket.ex
+++ lib/switchx/connection/socket.ex
@@ -1,21 +1,35 @@
 defmodule SwitchX.Connection.Socket do
+  @type switchx_event :: SwitchX.Event.new()
+
   @moduledoc """
   This modules provide to work with FreeSWITCH ESL socket
   """
 
   @doc """
   Given a port and a initial payload continues reading until it gets a full parsed
-  SwitchX.Event
+  SwitchX.Event.
   """
-  @spec recv(socket :: Port, payload :: String) :: SwitchX.Event.new()
+  @spec recv(socket :: Port, payload :: String) :: switchx_event
   def recv(socket, payload \\ "") when is_binary(payload) do
-    case :gen_tcp.recv(socket, 0) do
-      {:ok, "\n"} -> read_to_event(socket, SwitchX.Event.new(payload))
-      {:ok, data} -> recv(socket, payload <> data)
+    case :gen_tcp.recv(socket, 0, 1_000) do
+      # Initial header fully read, parsing event
+      {:error, :timeout} ->
+        read_body(socket, SwitchX.Event.new(payload))
+
+      {:ok, "\n"} ->
+        read_body(socket, SwitchX.Event.new(payload))
+
+      # Initial header fully read, parsing event
+      {:ok, data} ->
+        recv(socket, payload <> data)
     end
   end
 
-  defp read_to_event(socket, event) do
+  @doc """
+  Given a parsed initial event check if there is body to be read, and read them all.
+  """
+  @spec read_body(socket :: Port, event :: switchx_event) :: switchx_event
+  defp read_body(socket, event) do
     content_length = String.to_integer(Map.get(event.headers, "Content-Length", "0"))
 
     if content_length > 0 do
