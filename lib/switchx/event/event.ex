58e441304a78ed6e76368764aa0fd3419e9e01c6
Sergio Filipe <sergio.filipe@evolux.net.br>
2020-03-19 20:46:59 -0300

    Adding dump function to SwitchX.Event module

--- lib/switchx/event/event.ex
+++ lib/switchx/event/event.ex
@@ -39,4 +39,40 @@ defmodule SwitchX.Event do
     event = put_in(event.headers, Map.merge(event.headers, new.headers))
     put_in(event.body, event.body <> new.body)
   end
+
+  @doc false
+  def build({headers, body}) do
+    %__MODULE__{
+      headers: SwitchX.Event.Headers.new(headers).data,
+      body: Enum.join(body, "\n")
+    }
+  end
+
+  @doc """
+  Dumps a SwitchX.Event into a string URI encoded
+
+  Returns
+  ```
+    event_string
+  ```
+
+  ## Example
+
+      iex> SwitchX.Event.new(SwitchX.Event.Headers.new(%{foo: "bar 53"}), "body")
+           |> SwitchX.Event.dump()
+
+      "foo: bar%2053
+
+      body"
+  """
+  @spec dump(event :: SwitchX.Event) :: event_string :: String
+  def dump(event) do
+    h_part =
+      Enum.map(event.headers, fn {h, v} ->
+        "#{h}: #{if is_binary(v), do: URI.encode(v), else: v}"
+      end)
+      |> Enum.join("\n")
+
+    "#{h_part}\n\n#{event.body}"
+  end
 end
--- test/event/event_test.exs
+++ test/event/event_test.exs
@@ -76,4 +76,51 @@ defmodule SwitchX.Event.Test do
              }
            } = SwitchX.Event.new(message)
   end
+
+  test "Given an SwitchX.Event dump it into a String" do
+    message = """
+    Content-Length: 41
+    Content-Type: text/event-plain
+    Core-UUID: 42bdf272-16e6-11dd-b7a0-db4edd065621
+    Event-Calling-File: mod_event_socket.c
+    Event-Calling-Function: api_exec
+    Event-Calling-Line-Number: 609
+    Event-Date-GMT: Thu,%2001%20May%202008%2023:37:03%20GMT
+    Event-Date-Local: 2008-05-02%2007:37:03
+    Event-Date-timestamp: 1209685023894968
+    Event-Name: BACKGROUND_JOB
+    FreeSWITCH-Hostname: ser
+    FreeSWITCH-IPv4: 192.168.1.104
+    FreeSWITCH-IPv6: 127.0.0.1
+    Job-Command: originate
+    Job-Command-Arg: sofia/default/1005%20'&park'
+    Job-UUID: 7f4db78a-17d7-11dd-b7a0-db4edd065621
+
+    +OK 7f4de4bc-17d7-11dd-b7a0-db4edd065621
+    """
+
+    event = %SwitchX.Event{
+      body: "+OK 7f4de4bc-17d7-11dd-b7a0-db4edd065621\n",
+      headers: %{
+        "Content-Length" => 41,
+        "Content-Type" => "text/event-plain",
+        "Core-UUID" => "42bdf272-16e6-11dd-b7a0-db4edd065621",
+        "Event-Calling-File" => "mod_event_socket.c",
+        "Event-Calling-Function" => "api_exec",
+        "Event-Calling-Line-Number" => "609",
+        "Event-Date-GMT" => "Thu, 01 May 2008 23:37:03 GMT",
+        "Event-Date-Local" => "2008-05-02 07:37:03",
+        "Event-Date-timestamp" => "1209685023894968",
+        "Event-Name" => "BACKGROUND_JOB",
+        "FreeSWITCH-Hostname" => "ser",
+        "FreeSWITCH-IPv4" => "192.168.1.104",
+        "FreeSWITCH-IPv6" => "127.0.0.1",
+        "Job-Command" => "originate",
+        "Job-Command-Arg" => "sofia/default/1005 '&park'",
+        "Job-UUID" => "7f4db78a-17d7-11dd-b7a0-db4edd065621"
+      }
+    }
+
+    assert message == SwitchX.Event.dump(event)
+  end
 end
