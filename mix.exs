fbf71c71ae48b62d199fc8b6334ccdbb6e43939a
Sergio Filipe <sergio.filipe@evolux.net.br>
2022-08-01 11:32:28 -0300

    Bumbing version 0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SwitchX.MixProject do
   def project do
     [
       app: :switchx,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
